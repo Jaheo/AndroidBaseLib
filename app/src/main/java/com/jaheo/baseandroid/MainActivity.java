4ba06fa9c7a7222baef4f24b9f6000d44c3a818a
Jaheo <ma5074@163.com>
2021-10-21 17:42:36 +0800

    运行测试

--- app/src/main/java/com/jaheo/baseandroid/MainActivity.java
+++ app/src/main/java/com/jaheo/baseandroid/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.jaheo.baseandroid.exception.CrashHandler;
+import com.jaheo.baseandroid.log.LogTool;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CrashHandler.getInstance().init(this);
+        LogTool.initial(this);
+
     }
 }
